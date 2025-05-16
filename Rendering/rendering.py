65b2b3b9b41e1d32a7959212cc2fff792d6b35ba
Ulla1812 <1928080@stud.hs-mannheim.de>
2025-05-16 10:00:34 +0200

    few shot examples added

--- Rendering/rendering.py
+++ Rendering/rendering.py
@@ -4,4 +4,4 @@ def render_BPMN(bpmn_name,img_name):
     subprocess.run(["node", "Rendering/convert.js", bpmn_name, img_name],
                 check=True)
 
-render_BPMN("data/bpmn/three_agent_6.bpmn", "data/img/three_agent_6.png")
+render_BPMN("data/bpmn/three_agent_15.bpmn", "data/img/three_agent_15.png")
